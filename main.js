8b295bf5ad04c8ee8ab5e5345c83506aabed6012
nazaryan0909 <alenn0717@gmail.com>
2024-06-08 12:02:50 -0700

    Avelacrel em Varjutyun Z-n

--- main.js
+++ main.js
@@ -62,21 +62,67 @@
 
 // ե) մեծագույն և փոքրագույն տարրերի տարբերությունը,  
 
-function tarb(arr){
-    let max = arr[0];
-    for(let i = 0; i < arr.length; i++){
-        if(arr[i] > max){
-            max = arr[i]
-        }
-    }
-    let min = arr[0];
-    for(let i = 1; i < arr.length; i++){
-        if(arr[i] < min){
-            min = arr[i]
-        }
-    }
-    let tarberutyun = max - min
-    return tarberutyun
-}
-console.log(tarb([2, 7, 1, 8, 3]))
+// function tarb(arr){
+//     let max = arr[0];
+//     for(let i = 0; i < arr.length; i++){
+//         if(arr[i] > max){
+//             max = arr[i]
+//         }
+//     }
+//     let min = arr[0];
+//     for(let i = 1; i < arr.length; i++){
+//         if(arr[i] < min){
+//             min = arr[i]
+//         }
+//     }
+//     let tarberutyun = max - min
+//     return tarberutyun
+// }
+// console.log(tarb([2, 7, 1, 8, 3]))
 
+// զ) առաջին մեծագույն և վերջին փոքրագույն տարրերի միջև գտնվող անդամների գումարը,  
+// է) մեծագույն և մեծությամբ երկրորդ տարրերը,  
+// ը) դրական տարրերից փոքրագույնի արժեքը (եթե դրական տարր չկա, արտածել համապատասխան հաղորդագրություն):
+
+// զ) առաջին մեծագույն և վերջին փոքրագույն տարրերի միջև գտնվող անդամների գումարը,  
+
+let arr = [2,5,8,3,9,4]
+let indexMax = 0;
+let max = arr[0];
+for(let i = 0; i < arr.length; i++){
+    if(arr[i] > max){
+        max = arr[i]
+        indexMax = i
+
+    }
+
+}
+console.log(max)
+console.log(indexMax)
+
+let indexMin = arr.length-1
+let min = arr[arr.length-1]
+for(let i = arr.length-1; i >= 0; i--){
+    if(arr[i] < min){
+        min = arr[i]
+        indexMin = i
+    }
+}
+console.log(min)
+console.log(indexMin)
+
+if(indexMin < indexMax){
+    let sum = 0;
+    for(let i = indexMin + 1; i <= indexMax-1; i++){
+        sum = sum + arr[i]
+    }
+    console.log(sum)
+    
+}
+else {
+    let sum = 0;
+ for(let i = indexMax+1; i <= indexMin-1; i++){
+    sum = sum + arr[i]
+ }
+ console.log(sum)
+}
